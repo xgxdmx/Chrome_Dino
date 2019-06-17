03cf9562b6a2711af69f7180f953b33b4b345e47
石运 <shiyun19990127@gmail.com>
2019-06-17 15:20:58 +0800

    Version_3.1

--- Main.py
+++ Main.py
@@ -17,6 +17,7 @@ Grade = 100
 Probability_2 = 0
 Background_Music = ["./music/bg_music.mp3", "./music/bg_music2.mp3", "./music/bg_music3.mp3", "./music/bg_music4.mp3"]  # 背景音乐
 Music_Count = 0  # 背景音乐计数
+icon_img = "./images/favicon.ico"
 speed = 8  # 背景默认速度
 speed_plants = 8  # 植物默认速度
 speed_ptera = 5  # 翼龙默认速度
@@ -29,24 +30,24 @@ def Show_GameOver(screen, score_text, time_image):
     screen.fill(Background_Colour)
     GameOver_img = pygame.image.load('./images/modules/gameover.png').convert_alpha()
     GameOver_rect = GameOver_img.get_rect()
-    GameOver_rect.left, GameOver_rect.top = Width // 3, int(Height / 3.4)
+    GameOver_rect.left, GameOver_rect.top = Width // 2.75, int(Height / 3.4)
     screen.blit(GameOver_img, GameOver_rect)
     Restart_img = pygame.image.load("./images/modules/restart.png").convert_alpha()
     Restart_rect = Restart_img.get_rect()
-    Restart_rect.left, Restart_rect.top = int(Width / 2.25), int(Height / 2.3)
+    Restart_rect.left, Restart_rect.top = int(Width / 2.15), int(Height / 2.3)
     Difficult_Up_img = pygame.image.load("./images/modules/难度增加.png").convert_alpha()
     DifficultUp_rect = Difficult_Up_img.get_rect()
-    DifficultUp_rect.left, DifficultUp_rect.top = int(550), int(300)
+    DifficultUp_rect.left, DifficultUp_rect.top = int(530), int(300)
     Difficult_Down_img = pygame.image.load("./images/modules/难度降低.png").convert_alpha()
     DifficultDown_rect = Difficult_Down_img.get_rect()
-    DifficultDown_rect.left, DifficultDown_rect.top = int(50), int(300)
-    ChangeDino_img = pygame.image.load("./images/modules/更换角色.png").convert_alpha()
-    ChangeDino_rect = ChangeDino_img.get_rect()
-    ChangeDino_rect.left, ChangeDino_rect.top = int(310), int(300)
-    screen.blit(ChangeDino_img, ChangeDino_rect)
+    DifficultDown_rect.left, DifficultDown_rect.top = int(80), int(300)
+    ChangeBg_music = pygame.image.load("./images/modules/更换背景音乐.png").convert_alpha()
+    ChangeBg_music_rect = ChangeBg_music.get_rect()
+    ChangeBg_music_rect.left, ChangeBg_music_rect.top = int(270), int(300)
+    screen.blit(ChangeBg_music, ChangeBg_music_rect)
     screen.blit(Restart_img, Restart_rect)
-    screen.blit(score_text, [250, 250])  # 输出分数
-    screen.blit(time_image, [430, 250])
+    screen.blit(score_text, [250, 240])  # 输出分数
+    screen.blit(time_image, [430, 240])
     screen.blit(Difficult_Up_img, DifficultUp_rect)
     screen.blit(Difficult_Down_img, DifficultDown_rect)
     pygame.display.update()
@@ -73,29 +74,23 @@ def Show_GameOver(screen, score_text, time_image):
                 if DifficultUp_rect.right > mouse_pos[0] > DifficultUp_rect.left and mouse_pos[
                     1] < DifficultUp_rect.bottom and mouse_pos[1] > DifficultUp_rect.top:
                     Grade = Grade - 10
-
                     speed = speed + 1
                     speed_plants = speed_plants + 1
                     speed_ptera = speed_ptera + 1
-
                     score = 0
-
                     Obstache_Probability(score, Grade)
                     return True
                 if DifficultDown_rect.right > mouse_pos[0] > DifficultDown_rect.left and mouse_pos[
                     1] < DifficultDown_rect.bottom and mouse_pos[1] > DifficultDown_rect.top:
                     Grade = Grade + 10
-
                     speed = speed - 1
                     speed_plants = speed_plants - 1
                     speed_ptera = speed_ptera - 1
-
                     score = 0
-
                     Obstache_Probability(score, Grade)
                     return True
-                if ChangeDino_rect.right > mouse_pos[0] > ChangeDino_rect.left and mouse_pos[
-                    1] < ChangeDino_rect.bottom and mouse_pos[1] > ChangeDino_rect.top:
+                if ChangeBg_music_rect.right > mouse_pos[0] > ChangeBg_music_rect.left and mouse_pos[
+                    1] < ChangeBg_music_rect.bottom and mouse_pos[1] > ChangeBg_music_rect.top:
                     if Music_Count < 4:
                         Music_Count += 1
                     if Music_Count > 3:
@@ -118,6 +113,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((Width, Height))
     pygame.display.set_caption("梦の彼方")
+    ico = pygame.image.load(icon_img).convert_alpha()
+    pygame.display.set_icon(ico)
     Clock = pygame.time.Clock()
     # 得分初始化
     score = 0
@@ -158,13 +155,13 @@ def main():
         time_sec = left_time % 60000 // 1000
         time_text = str(time_min).zfill(2) + ':' + str(time_sec).zfill(2)
         time_font = pygame.font.Font("./fonts/FZSJ-TXJW.TTF", 30)
-        time_image = time_font.render(time_text, True, (0, 0, 0))
+        time_image = time_font.render("Time：" + time_text, True, (0, 0, 0))
         screen.blit(time_image, [600, 10])
         # 显示得分
         score_text = font.render("Score：" + str(score), 1, (0, 0, 0))
-        screen.blit(score_text, [20, 10])
+        screen.blit(score_text, [30, 10])
         Probability_text = font.render("障碍物概率：" + str(Probability_2), 1, (0, 0, 0))
-        screen.blit(Probability_text, [200, 10])
+        screen.blit(Probability_text, [210, 10])
 
         pygame.display.flip()
         Clock.tick(60)
